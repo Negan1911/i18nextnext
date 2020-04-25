0fb64bbc98d6807d05cef3517a5cdd29fc916bde
Nahuel Veron <nahuelav96@gmail.com>
2020-04-25 03:11:12 -0300

    Adding i18next changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { i18n } from 'i18next';
-import { getI18n, I18nextProvider, setI18n } from 'react-i18next'
+import { I18nextProvider, getI18n, setI18n } from 'react-i18next'
 
 type LocalesProviderProps = { i18n: i18n, children: React.ReactElement }
 
-export function LocalesProvider({ children, i18n }: LocalesProviderProps) {
+export function NextNextProvider({ children, i18n }: LocalesProviderProps) {
   setI18n(i18n)
   if (globalThis.store) {
     i18n.services.resourceStore.data = globalThis.store
@@ -28,7 +28,10 @@ export function LocaleScript() {
 
 export async function LoadLocales(lng: string, namespaces: string[]) {
   const i18n = getI18n()
+  if (!i18n) {
+    throw new Error('i18n has not been set')
+  }
+
   await i18n.reloadResources(lng, namespaces);
   globalThis.store = i18n.services.resourceStore.data
-  return
 }
